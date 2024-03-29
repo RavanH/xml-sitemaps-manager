bb83b33cf508d1883a7721ce9e6ae2fd3a75e1c0
RavanH <ravanhagen@gmail.com>
2024-03-29 02:40:47 +0100

    fix autloader

--- xml-sitemaps-manager.php
+++ xml-sitemaps-manager.php
@@ -6,7 +6,7 @@
  * Plugin URI:        https://status301.net/wordpress-plugins/xml-sitemaps-manager/
  * Description:       Fix some bugs and add new options to manage the WordPress core XML Sitemaps. Happy with the results? Please leave me a <strong><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=XML%20Sitemaps%20Manager">tip</a></strong> for continued development and support. Thanks :)
  * Text Domain:       xml-sitemaps-manager
- * Version:           0.6-alpha7
+ * Version:           0.6-beta1
  * Requires at least: 5.5
  * Requires PHP:      5.6
  * Author:            RavanH
@@ -17,7 +17,7 @@
 
 defined( 'WPINC' ) || die;
 
-define( 'WPSM_VERSION', '0.6-alpha7' );
+define( 'WPSM_VERSION', '0.6-beta1' );
 
 /**
  * Plugin intitialization.
@@ -207,7 +207,7 @@ function xmlsm_autoloader( $class_name ) {
 	$class_name = str_replace( 'XMLSitemapsManager', 'src', $class_name );
 	$path_array = explode( '\\', $class_name );
 	$file_name  = array_pop( $path_array );
-	$file_name  = 'class.' . strtolower( $file_name ) . '.php';
+	$file_name  = 'class-' . strtolower( $file_name ) . '.php';
 	$file       = __DIR__ . DIRECTORY_SEPARATOR . implode( DIRECTORY_SEPARATOR, $path_array ) . DIRECTORY_SEPARATOR . $file_name;
 
 	// If the file exists, inlcude it.
