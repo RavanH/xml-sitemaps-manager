b502a70dd2a03d7ddf495467d867f4409c406bc5
RavanH <ravanhagen@gmail.com>
2024-04-01 02:59:09 +0200

    namespace autiloader

--- includes/autoload.php
+++ includes/autoload.php
@@ -7,6 +7,8 @@
  * @since 0.7
  */
 
+namespace XMLSitemapsManager;
+
 /**
  * XML Sitemap Manager Autoloader.
  *
@@ -16,24 +18,24 @@
  *
  * @return void
  */
-function xmlsm_autoloader( $class_name ) {
+function autoloader( $class_name ) {
 	// Skip this if not in our namespace.
-	if ( 0 !== strpos( $class_name, 'XMLSitemapsManager' ) ) {
+	if ( 0 !== \strpos( $class_name, __NAMESPACE__ ) ) {
 		return;
 	}
 
 	// Replace namespace separators with directory separators in the relative
 	// class name, prepend with class-, append with .php, build our file path.
-	$class_name = str_replace( 'XMLSitemapsManager', '', $class_name );
-	$class_name = strtolower( $class_name );
-	$path_array = explode( '\\', $class_name );
-	$file_name  = 'class-' . array_pop( $path_array ) . '.php';
-	$file       = __DIR__ . implode( DIRECTORY_SEPARATOR, $path_array ) . DIRECTORY_SEPARATOR . $file_name;
+	$class_name = \str_replace( __NAMESPACE__, '', $class_name );
+	$class_name = \strtolower( $class_name );
+	$path_array = \explode( '\\', $class_name );
+	$file_name  = 'class-' . \array_pop( $path_array ) . '.php';
+	$file       = __DIR__ . \implode( \DIRECTORY_SEPARATOR, $path_array ) . \DIRECTORY_SEPARATOR . $file_name;
 
 	// If the file exists, inlcude it.
-	if ( file_exists( $file ) ) {
+	if ( \file_exists( $file ) ) {
 		include $file;
 	}
 }
 
-spl_autoload_register( 'xmlsm_autoloader' );
+\spl_autoload_register( __NAMESPACE__ . '\autoloader' );
