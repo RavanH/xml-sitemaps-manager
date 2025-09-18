884a637ef921218d564d2f07344ef00f40d20ef8
RavanH <ravanhagen@gmail.com>
2025-09-18 23:06:19 +0200

    fix autoloader and plugin version

--- xml-sitemaps-manager.php
+++ xml-sitemaps-manager.php
@@ -6,7 +6,7 @@
  * Plugin URI:        https://status301.net/wordpress-plugins/xml-sitemaps-manager/
  * Description:       Fix some bugs and add new options to manage the WordPress core XML Sitemaps. Happy with the results? Please leave me a <strong><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=XML%20Sitemaps%20Manager">tip</a></strong> for continued development and support. Thanks :)
  * Text Domain:       xml-sitemaps-manager
- * Version:           0.7-alpha5
+ * Version:           0.7
  * Requires at least: 5.5
  * Requires PHP:      5.6
  * Author:            RavanH
@@ -17,7 +17,7 @@
 
 defined( 'WPINC' ) || die;
 
-define( 'XMLSM_VERSION', '0.7-alpha5' );
+define( 'XMLSM_VERSION', '0.7' );
 define( 'XMLSM_BASENAME', plugin_basename( __FILE__ ) );
 
 add_action( 'init', array( 'XMLSitemapsManager\Load', 'front' ), 9 );
@@ -37,17 +37,17 @@ register_deactivation_hook( __FILE__, array( 'XMLSitemapsManager\Admin', 'deacti
 \spl_autoload_register(
     function ( $class_name ) {
         // Skip this if not in our namespace.
-        if ( 0 !== \strpos( $class_name, __NAMESPACE__ ) ) {
+        if ( 0 !== \strpos( $class_name, 'XMLSitemapsManager\\' ) ) {
             return;
         }
 
         // Replace namespace separators with directory separators in the relative
         // class name, prepend with class-, append with .php, build our file path.
-        $class_name = \str_replace( __NAMESPACE__, '', $class_name );
+        $class_name = \str_replace( 'XMLSitemapsManager', 'includes', $class_name );
         $class_name = \strtolower( $class_name );
         $path_array = \explode( '\\', $class_name );
         $file_name  = 'class-' . \array_pop( $path_array ) . '.php';
-        $file       = __DIR__ . \implode( \DIRECTORY_SEPARATOR, $path_array ) . \DIRECTORY_SEPARATOR . $file_name;
+        $file       = __DIR__ .\DIRECTORY_SEPARATOR . \implode( \DIRECTORY_SEPARATOR, $path_array ) . \DIRECTORY_SEPARATOR . $file_name;
 
         // If the file exists, inlcude it.
         if ( \file_exists( $file ) ) {
